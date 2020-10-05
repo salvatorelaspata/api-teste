045162391b0d8bc11270bd2455f8148edfb75a08
salvatore <salvatore.la.spata@gmail.com>
2020-10-05 13:54:46 +0200

    err

--- express/server.js
+++ express/server.js
@@ -49,6 +49,13 @@ router.get("/read_components", (req, res) => {
     res.send(layer);
   });
 
+  process.stdout.on("error", function (err) {
+    console.log("error", err);
+    if (err.code == "EPIPE") {
+      process.exit(0);
+    }
+  });
+
   py.stdin.write(JSON.stringify(data));
 
   py.stdin.end();
