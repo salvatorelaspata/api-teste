112ff9bd5a42e0a6209b931423ab9e8c70a1dfcd
salvatore <salvatore.la.spata@gmail.com>
2020-10-05 11:11:40 +0200

    work around

--- express/server.js
+++ express/server.js
@@ -12,26 +12,41 @@ router.get("/", (req, res) => {
   res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
+const { PythonShell } = require("python-shell");
+
+let options = {
+  mode: "text",
+  pythonPath: "python",
+  pythonOptions: ["-u"], // get print results in real-time
+  scriptPath: "./",
+  args: ["img.psd"],
+};
 
 router.get("/read_components", (req, res) => {
-  const spawn = require("child_process").spawn,
-    py = spawn("python", ["read_layer.py", "img.psd"]);
-  let dataString = "";
+  // const spawn = require("child_process").spawn,
+  //   py = spawn("python", ["read_layer.py", "img.psd"]);
+  // let dataString = "";
 
-  py.stdout.on("data", function (data) {
-    dataString += data.toString();
+  // py.stdout.on("data", function (data) {
+  //   dataString += data.toString();
+  // });
+
+  // py.stdout.on("end", function () {
+  //   if (!dataString) return;
+  //   var layer = JSON.parse(dataString);
+  //   console.log(layer);
+  //   py.stdin.pause();
+  //   py.stdin.destroy();
+  //   res.send("C SEMU!");
+  // });
+
+  // py.stdin.end();
+  PythonShell.run("read_layer.py", options, function (err, results) {
+    if (err) console.log(err);
+    // results is an array consisting of messages collected during execution
+    res.send(results);
+    console.log("results: %j", results);
   });
-
-  py.stdout.on("end", function () {
-    if (!dataString) return;
-    var layer = JSON.parse(dataString);
-    console.log(layer);
-    py.stdin.pause();
-    py.stdin.destroy();
-    res.send("C SEMU!");
-  });
-
-  py.stdin.end();
   //py.stdin.write(JSON.stringify(data));
 });
 
@@ -43,7 +58,7 @@ app.use("/.netlify/functions/server", router); // path must route to lambda
 app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 // DEBUG
-// app.listen(3000, () => console.log("Local app listening on port 3000!"));
+app.listen(3000, () => console.log("Local app listening on port 3000!"));
 
 module.exports = app;
 module.exports.handler = serverless(app);
