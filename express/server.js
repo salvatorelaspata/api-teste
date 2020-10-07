e45d9031fded61a73c750d564f6657719fc5ef49
salvatore <salvatore.la.spata@gmail.com>
2020-10-07 20:42:12 +0200

    3

--- express/server.js
+++ express/server.js
@@ -87,6 +87,59 @@ router.get("/read_components", (req, res) => {
 
   // read(res);
 });
+
+router.get("/read_components2", (req, res) => {
+  console.log("/read_components");
+  var dataToSend;
+  // inspect the PATH key on the env object
+  console.log(process.env.PATH);
+
+  const python = spawn("python2.7.12", ["--version"]);
+  // collect data from script
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.send(dataToSend);
+  });
+
+  python.on("error", (code) => {
+    console.log(`ERRORRRRRRR ${code}`);
+    // send data to browser
+    res.send(code);
+  });
+});
+
+router.get("/read_components3", (req, res) => {
+  console.log("/read_components");
+  var dataToSend;
+  // inspect the PATH key on the env object
+  console.log(process.env.PATH);
+
+  const python = spawn("python3.5.2", ["--version"]);
+  // collect data from script
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.send(dataToSend);
+  });
+
+  python.on("error", (code) => {
+    console.log(`ERRORRRRRRR ${code}`);
+    // send data to browser
+    res.send(code);
+  });
+});
+
 /*
 const read = (res) => 
 {
